229d5ec8546f6145fde175c244cf637a924d4045
GoncaloC25 <correiagoncalo@ipvc.pt>
2025-06-28 00:19:46 +0100

    Correção ficheiro em falta backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
 // API real a ser implementada
+
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const alunoRoute = require('./routes/alunoRoutes');
+const cursoRoute = require('./routes/cursoRoutes');
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/alunos', alunoRoute); 
+app.use('/cursos', cursoRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
