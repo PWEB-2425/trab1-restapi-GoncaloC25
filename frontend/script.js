bc80dcba43f080bdc8efa13fdaf2912b2941db7f
GoncaloC25 <correiagoncalo@ipvc.pt>
2025-06-28 11:19:28 +0100

    Revert "correção bug implementação backend e frontend"
    This reverts commit 712bbff495507eb3d6b9586f59c26c5a1db8b13f.

--- frontend/script.js
+++ frontend/script.js
@@ -26,11 +26,15 @@ async function listarAlunos(){
     const cursosresposta = await fetch(cursosurl);
 
     const alunosJS = await alunosresposta.json();
+<<<<<<< HEAD
     const alunosArray = alunosJS[0]?.alunos ?? [];
 
     const cursosJS = await cursosresposta.json();
     const cursosArray = cursosJS[0]?.cursos ?? [];
     console.log(cursosJS);
+=======
+    const cursosJS = await cursosresposta.json();
+>>>>>>> parent of 712bbff (correção bug implementação backend e frontend)
 
     table.innerHTML = "";
 
@@ -222,7 +226,10 @@ async function editarAluno(evento) {
 async function makeInptus(row){
     const cursosresposta = await fetch(cursosurl);
     const cursosJS = await cursosresposta.json();
+<<<<<<< HEAD
     const cursosArray = cursosJS[0]?.cursos ?? [];
+=======
+>>>>>>> parent of 712bbff (correção bug implementação backend e frontend)
 
     let namedata = document.createElement("td");
     let nomeInput = document.createElement("input");
@@ -294,11 +301,17 @@ function mostraCursos() {
 async function listarCurso(){
     const cursosresposta = await fetch(cursosurl);
     const cursosJS = await cursosresposta.json();
+<<<<<<< HEAD
     const cursosArray = cursosJS[0]?.cursos ?? [];
 
     const alunosresposta = await fetch(alunosurl);
     const alunosJS = await alunosresposta.json();
     const alunosArray = alunosJS[0]?.alunos ?? [];
+=======
+
+    const alunosresposta = await fetch(alunosurl);
+    const alunosJS = await alunosresposta.json();
+>>>>>>> parent of 712bbff (correção bug implementação backend e frontend)
 
     table.innerHTML = "";
     table.innerHTML = "<thead><tr><th>Nome do Curso</th><th>Alunos Inscritos</th><th>Ações</th></tr></thead>";
@@ -438,7 +451,13 @@ async function editarCurso(evento) {
     });
 }
 
+<<<<<<< HEAD
 function makeCursoInputs(row){
+=======
+async function makeCursoInputs(row){
+    const cursosresposta = await fetch(cursosurl);
+    const cursosJS = await cursosresposta.json();
+>>>>>>> parent of 712bbff (correção bug implementação backend e frontend)
 
     let namedata = document.createElement("td");
     let nomeInput = document.createElement("input");
