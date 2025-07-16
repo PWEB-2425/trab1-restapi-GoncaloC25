83bdb55583ba606903a200fc106d9524de4355ae
Gonçalo Correia <118832307+GoncaloC25@users.noreply.github.com>
2025-07-17 00:30:09 +0100

    backend debug

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.set('trust proxy', 1);
 
 app.use(cors({
-    origin: 'https://trab1-pw-frontend-gray.vercel.app', // Frontend URL
+    origin: 'https://trab1-pw-frontend-gray.vercel.app/*', // Frontend URL
     credentials: true
 }));
 
